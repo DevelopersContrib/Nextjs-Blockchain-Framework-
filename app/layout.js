241a4831c270b388def995232b25fed1566e7e6d
joseph al <sephjavier@gmail.com>
2023-07-10 11:45:53 +0800

    default meta tag values

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,12 @@ import { getData } from '@/lib/data'
 
 export async function generateMetadata() {
   const c = await getData();
-
+  console.log(c.data.author);
 	return {
-		title: c.data.title===''?'Welcome to '+c.data.domainName:c.data.title,
-		description: c.data.description,
-    keywords: c.data.keywords,
-    author: c.data.author
+		title: c.data.title==='' || c.data.title === null ?'Welcome to '+c.data.domainName:c.data.title,
+		description: c.data.description === '' || c.data.description === null ? 'Join a vibrant community of developers, influencers, and entrepreneurs on '+c.data.domainName+', all using the versatile CONTRIB token to power their token economies!':c.data.description,
+    keywords: c.data.keywords == '' || c.data.keywords === null ? 'website': c.data.keywords,
+    author: c.data.author  == '' || c.data.author === null ? 'contrib': c.data.author
 	}
 }
 
