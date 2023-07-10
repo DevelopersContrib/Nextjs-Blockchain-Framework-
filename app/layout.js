b31c369947a5684ef8228851904770fc9e4dc0be
joseph al <sephjavier@gmail.com>
2023-07-10 10:41:09 +0800

    add metatags

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,17 @@
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getData } from '@/lib/data'
+
+export async function generateMetadata() {
+  const c = await getData();
+
+	return {
+		title: c.data.title===''?'Welcome to '+c.data.domainName:c.data.title,
+		description: c.data.description,
+    keywords: c.data.keywords,
+    author: c.data.author
+	}
+}
 
 export default function RootLayout({ children }) {
   return (
