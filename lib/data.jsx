7667bd14bb00513619adbabd51b9e07e38afb360
Kareen <kjabellar@gmail.com>
2024-05-30 11:59:52 +0800

    added caching

--- lib/data.jsx
+++ lib/data.jsx
@@ -13,7 +13,7 @@ export function getDomain() {
 export async function getData() {
   const domain = getDomain();
   const url = process.env.CONTRIB_API1+`&domain=${domain}`
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 3600 } });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
